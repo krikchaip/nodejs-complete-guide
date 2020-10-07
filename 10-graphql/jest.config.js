9619a9bc2600afa257b2554df83e58b2c5eaaa9e
Eventpop <eventpop@krikchaip-evp.local>
2020-10-07 17:17:14 +0700

    10-graphql: folder scaffold

---
+++ 10-graphql/jest.config.js
@@ -0,0 +1,13 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+
+const { compilerOptions } = require('./tsconfig.json')
+
+/** @typedef {import('ts-jest')} */
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths)
+}
+
+module.exports = config
