447cac7c2817685b4c919f22670680f729202849
r3nniw <r3nniw@r3nniw-15.local>
2020-11-01 13:24:21 +0700

    10-graphql: sequelize log highlight statement

--- 10-graphql/config/sequelize.ts
+++ 10-graphql/config/sequelize.ts
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize'
 
+import color from '@lib/console-color'
+
 // TODO custom config options based on NODE_ENV
-// TODO highlight SQL statements
 const sequelize = new Sequelize({
   host: 'db',
   port: 5432,
@@ -12,10 +13,18 @@ const sequelize = new Sequelize({
   timezone: '+07:00',
   typeValidation: true,
   define: { freezeTableName: true, timestamps: false },
-  logging: sql =>
+  logging: sql => {
+    const date = new Date()
+    date.setUTCHours(date.getUTCHours() + 7)
     console.log(
-      `\x1b[1m${new Date().toUTCString()}\x1b[0m\n\x1b[36m${sql}\x1b[0m\n`
+      `${color.bold}${date.toLocaleString('th')}${color.end}\n${
+        color.cyan
+      }${sql.replace(
+        /\b([A-Z]{2,})\b/g,
+        `${color.end}${color.magenta}$1${color.end}${color.cyan}`
+      )}${color.end}\n`
     )
+  }
 })
 
 export default sequelize
---
+++ 10-graphql/lib/console-color.ts
@@ -0,0 +1,6 @@
+export default {
+  bold: '\x1b[1m',
+  magenta: '\x1b[35m',
+  cyan: '\x1b[36m',
+  end: '\x1b[0m'
+}
